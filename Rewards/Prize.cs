88e9b74bfab9262a0a4d690d32c8646c70d0a4a8
bshn <bshn@FRANK-PC>
2016-10-11 09:35:04 +0800

    添加项目文件。

---
+++ Rewards/Prize.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rewards
+{
+    /// <summary>
+    /// 抽奖
+    /// </summary>
+    public class Prize
+    {
+        /// <summary>
+        /// 奖品关键字
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 权重/数量
+        /// </summary>
+        public int Poll { get; set; }
+
+
+        /// <summary>
+        /// 中奖区间
+        /// </summary>
+        class Area
+        {
+            /// <summary>
+            /// 奖品关键字
+            /// </summary>
+            public string Key { get; set; }
+
+            /// <summary>
+            /// 开始索引位置
+            /// </summary>
+            public int Start { get; set; }
+
+            /// <summary>
+            /// 截止索引位置
+            /// </summary>
+            public int Over { get; set; }
+        }
+
+        /// <summary>
+        /// 随机种子
+        /// </summary>
+        static Random Rand = new Random((int)DateTime.Now.Ticks);
+
+
+        /// <summary>
+        /// 轮盘抽奖，权重值(在轮盘中占的面积大小)为中奖几率
+        /// </summary>
+        /// <param name="prizeList">礼品列表（如果不是百分百中奖则轮空需要加入到列表里面）</param>
+        /// <returns></returns>
+        public static string Roulette(List<Prize> prizeList)
+        {
+            if (prizeList == null || prizeList.Count == 0) return string.Empty;
+            if (prizeList.Any(x => x.Poll < 1)) throw new ArgumentOutOfRangeException("poll权重值不能小于1");
+            if (prizeList.Count == 1) return prizeList[0].Key; //只有一种礼品
+
+            Int32 total = prizeList.Sum(x => x.Poll); //权重和               
+            if (total > 1000) throw new ArgumentOutOfRangeException("poll权重和不能大于1000"); //数组存储空间的限制。最多一千种奖品（及每种奖品的权重值都是1）
+
+            List<int> speed = new List<int>(); //随机种子
+            for (int i = 0; i < total; i++) speed.Add(i);
+
+            int pos = 0;
+            Dictionary<int, string> box = new Dictionary<int, string>();
+            foreach (Prize p in prizeList)
+            {
+                for (int c = 0; c < p.Poll; c++) //权重越大所占的面积份数就越多
+                {
+                    pos = Prize.Rand.Next(speed.Count); //取随机种子坐标
+                    box[speed[pos]] = p.Key; //乱序 礼品放入索引是speed[pos]的箱子里面
+                    speed.RemoveAt(pos); //移除已抽取的箱子索引号
+                }
+            }
+            return box[Prize.Rand.Next(total)];
+        }
+
+        /// <summary>
+        /// 奖盒抽奖，每个参与者对应一个奖盒，多少人参与就有多少奖盒
+        /// </summary>
+        /// <param name="prizeList">礼品列表</param>
+        /// <param name="peopleCount">参与人数</param>
+        /// <returns></returns>
+        public static string LunkyBox(List<Prize> prizeList, int peopleCount)
+        {
+            if (prizeList == null || prizeList.Count == 0) return string.Empty;
+            if (prizeList.Any(x => x.Poll < 1)) throw new ArgumentOutOfRangeException("poll礼品数量不能小于1个");
+            if (peopleCount < 1) throw new ArgumentOutOfRangeException("参数人数不能小于1人");
+            if (prizeList.Count == 1 && peopleCount <= prizeList[0].Poll) return prizeList[0].Key; //只有一种礼品且礼品数量大于等于参与人数
+
+            int pos = 0;
+            List<Area> box = new List<Area>();
+            foreach (Prize p in prizeList)
+            {
+                box.Add(new Area() { Key = p.Key, Start = pos, Over = pos + p.Poll }); //把礼品放入奖盒区间
+                pos = pos + p.Poll;
+            }
+
+            int total = prizeList.Sum(x => x.Poll); //礼品总数
+            int speed = Math.Max(total, peopleCount); //取礼品总数和参数总人数中的最大值
+
+            pos = Prize.Rand.Next(speed);
+            Area a = box.FirstOrDefault(x => pos >= x.Start && pos < x.Over); //查找索引在奖盒中对应礼品的位置
+
+            return a == null ? string.Empty : a.Key;
+        }
+
+    }
+
+
+    /*
+    List<Prize> prizes = new List<Prize>();
+    prizes.Add(new Prize() { Key = "电脑", Poll = 1 });
+    prizes.Add(new Prize() { Key = "机柜", Poll = 2 });
+    prizes.Add(new Prize() { Key = "鼠标", Poll = 3 });
+
+
+    string lp1 = Prize.LunkyBox(prizes, 6);
+    Console.WriteLine(lp1);
+
+
+    prizes.Add(new Prize() { Key = "谢谢惠顾", Poll = 5 });
+    string lp2 = Prize.Roulette(prizes);      
+    Console.WriteLine(lp2);
+    */
+}
---
+++ Rewards/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Rewards
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //GetRewards();
+
+            List<Prize> prizes = new List<Prize>();
+            //prizes.Add(new Prize() { Key = "电脑", Poll = 1 });
+            //prizes.Add(new Prize() { Key = "机柜", Poll = 2 });
+            //prizes.Add(new Prize() { Key = "鼠标", Poll = 3 });
+            //prizes.Add(new Prize() {Key = "小白.手机壳1个",Poll = 0});
+            //prizes.Add(new Prize() { Key = "谢谢惠顾", Poll = 5 });
+
+
+            prizes.Add(new Prize() { Key = "魔豆50个					", Poll = 5 });
+            prizes.Add(new Prize() { Key = "魔豆10个					", Poll = 10 });
+            prizes.Add(new Prize() { Key = "魔豆100 个				", Poll = 3 });
+            prizes.Add(new Prize() { Key = "魔衣50元					", Poll = 5 });
+            prizes.Add(new Prize() { Key = "魔法商店全场20元			", Poll = 10 });
+            prizes.Add(new Prize() { Key = "高端肖像画.无边框油画 200元", Poll = 5 });
+            prizes.Add(new Prize() { Key = "高端肖像画 100元			", Poll = 8 });
+            prizes.Add(new Prize() { Key = "桌上肖像画 50元			", Poll = 13 });
+            prizes.Add(new Prize() { Key = "星座杯 30元				", Poll = 15 });
+            prizes.Add(new Prize() { Key = "手机系列 50元				", Poll = 13 });
+            prizes.Add(new Prize() { Key = "惊喜奖					", Poll = 3 });
+            prizes.Add(new Prize() { Key = "心情魔镜.手机背贴1个		", Poll = 0 });
+            prizes.Add(new Prize() { Key = "小魔飞吻1个,么么哒~		", Poll = 10 });
+            prizes.Add(new Prize() { Key = "小白.手机壳1个			", Poll = 0 });
+            prizes.Add(new Prize() { Key = "谢谢惠顾", Poll = 50 });
+
+            //string lp1 = Prize.LunkyBox(prizes, 6);
+            //Console.WriteLine(lp1);
+
+            for (int i = 0; i < 100; i++)
+            {
+                
+                string lp2 = Prize.Roulette(prizes.Where(t => t.Poll > 0).ToList());
+                Console.WriteLine(lp2);
+            }
+
+            Console.Read();
+        }
+
+        private static void GetRewards()
+        {
+            //各物品的概率保存在数组里
+            float[] area = new float[]
+            {
+                0.980f,
+                0.550f,
+                0.230f, 0.230f, 0.230f, 0.230f, 0.230f,
+                0.010f
+            };
+
+            //单次测试
+            //Console.WriteLine(Get(area));
+
+            //批量测试
+            int[] result = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < 100; i++) //为了比对结果方便，这里循环的次数是总概率的1000倍
+            {
+                int n = Get(area); //本次抽奖结果
+                //Console.CursorLeft = 0;
+                //Console.CursorTop = 0;
+                Thread.Sleep(100);
+                Console.WriteLine(n);
+                result[n]++; //统计抽到的次数
+            }
+            Console.WriteLine("结果：");
+            foreach (int times in result)
+            {
+                Console.WriteLine(times);
+            }
+        }
+
+        /// <summary>
+        /// 获取抽奖结果
+        /// </summary>
+        /// <param name="prob">各物品的抽中概率</param>
+        /// <returns>返回抽中的物品所在数组的位置</returns>
+        private static int Get(float[] prob)
+        {
+            int result = 0;
+            var sum = prob.Sum();
+            int n = (int)(sum * 1000);           //计算概率总和，放大1000倍
+            Random r = rnd;
+            float x = (float)r.Next(0, n) / 1000;       //随机生成0~概率总和的数字
+
+            for (int i = 0; i < prob.Count(); i++)
+            {
+                float pre = prob.Take(i).Sum();         //区间下界
+                float next = prob.Take(i + 1).Sum();    //区间上界
+                if (x >= pre && x < next)               //如果在该区间范围内，就返回结果退出循环
+                {
+                    result = i;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        private static Random rnd = new Random();
+    }
+}
